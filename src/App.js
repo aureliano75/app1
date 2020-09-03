c52b00162066ae6c3d53b45c4641d6d8a091ab7e
aureliano75 <61304559+aureliano75@users.noreply.github.com>
2020-09-03 15:22:28 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,35 +2,52 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
-    <ProgressBar
-      redbar={redbar}
-      greenbar={greenbar}
-      meter={meter}
-      percentage="66%"
-    />
-  );
-}
-
-function ProgressBar(props){
-  var percentage = props.percentage;
-  props.greenbar.width = percentage;
-  var remaining = (1 - parseFloat(props.greenbar.width)/100).toLocaleString("en", {style: "percent"});
-  props.redbar.width = remaining;
-  return(
-    <div style={props.meter}>
-      <div style={props.greenbar}>
-        Completion {props.greenbar.width}
-      </div>
-      <div style={props.redbar}>
-        Remaining {remaining}
-      </div>
-    </div>
+    <Container />
+    
     
   );
-
 }
+
+function Container(){
+  return (
+    <div style={container}>
+      <ProgressBar
+        redbar={redbar}
+        greenbar={greenbar}
+        meter={meter}
+        percentage="66%"
+      />
+    </div>
+  )
+}
+
+class ProgressBar extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {};
+    var percentage = props.percentage;
+    props.greenbar.width = percentage;
+    var remaining = (1 - parseFloat(props.greenbar.width)/100).toLocaleString("en", {style: "percent"});
+    props.redbar.width = remaining;
+  }
+
+  render(){
+    return(
+    <div style={this.props.meter}>
+      <div style={this.props.greenbar}>
+        Completion this.{this.props.greenbar.width}
+      </div>
+      <div style={this.props.redbar}>
+        Remaining {this.remaining}
+      </div>
+    </div>
+    );
+  }
+}
+
 
 const reactstart = (
 <div className="App">
@@ -63,14 +80,20 @@ const greenbar = {
   "border-radius": "10px 0px 0px 10px",
   display: "inline-block",
   backgroundColor: "#42f578",
-  width: "%"
+  width: ""
 };
 
 const meter = {
   "border-radius": "10px",
   border: "solid black 1px",
-  margin: "5px"
+  margin: "5px",
 };
+
+const container = {
+  border:"1px solid black",
+  width: "80%",
+  margin: "auto"
+}
 
 const loginpage = (
 <div style={redbar}>
