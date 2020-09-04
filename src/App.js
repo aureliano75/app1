c2977798e2b85859ad81449d637689b405c08386
aureliano75 <61304559+aureliano75@users.noreply.github.com>
2020-09-04 14:11:18 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import './App.css';
+import Chart from 'chart.js';
 
 
 function App() {
@@ -18,10 +19,18 @@ function H1(props){
   );
 }
 
+const chartdataA = [0, 10, 5, 2, 20, 30, 45, 65, 71];
+const chartdataB = [5, 15, 20, 7, 25, 39, 55, 75, 91];
+const chartdata1A = [0, 10, 5, 2, 20, 30, 45, 65, 71];
+const chartdata1B = [5, 15, 20, 7, 25, 39, 55, 75, 91];
+const chartdata2A = [0, 10, 5, 2, 20, 30, 45, 65, 71];
+const chartdata2B = [5, 15, 20, 7, 25, 39, 55, 75, 91];
+
+
 function Container(){
   return (
     <div style={container}>
-      <H1 
+      {/* <H1 
         text={"Hello World"}  
       />
       <ul>
@@ -41,15 +50,43 @@ function Container(){
         todate="11"
         total="300"
       />
-      <ProgressBarV2 
+      */}
+      {/* <ProgressBarV2 
         bar2={bluebar}
         bar1={orangebar}
         meter={meter}
-        todate={11}
+        todate={10}
         total="300"
-      />
+      />  */}
 
-      
+      <table style={{margin:"auto"}}>
+        <tr>
+          <td colSpan="2">
+            <canvas id="myChart" width="1000" height="300" style={{position:"relative",left:"0px"}}></canvas>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <canvas id="myChart1" width="500" height="300" ></canvas>
+          </td>
+          <td>
+            <canvas id="myChart1.1" width="500" height="300" ></canvas>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <canvas id="myChart2" width="500" height="300"></canvas>
+          </td>
+          <td>
+            <canvas id="myChart2.1" width="500" height="300"></canvas>
+          </td>
+        </tr>
+      </table>
+      <Graph id="myChart" type="bar" color1="red" color2="#8ceb34" title="CCL Sioux Falls 1" data1 = {chartdataA} data2 = {chartdataB}/>
+      <Graph id="myChart1" type="horizontalBar" color1="yellow" color2="purple" title="Johnson & Johnson" data1 = {chartdata1A} data2 = {chartdata1B}/>
+      <Graph id="myChart1.1" type="line" color1="yellow" color2="purple" title="Johnson & Johnson" data1 = {chartdata1A} data2 = {chartdata1B}/>
+      <Graph id="myChart2" type="horizontalBar" color1="orange" color2="blue" title="P&G" data1 = {chartdata2A} data2 = {chartdata2B}/>
+      <Graph id="myChart2.1" type="line" color1="orange" color2="blue" title="P&G" data1 = {chartdata2A} data2 = {chartdata2B}/>
 
     </div>
   )
@@ -61,6 +98,65 @@ const list = listelements.map((listelement)=>
 <li>{listelement}</li>
 );
 
+class Graph extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  componentDidMount() {
+    this.doit();
+    this.interval = setInterval(() => this.doit(), 100);
+  }
+
+
+  doit(){
+    var ctx1 = document.getElementById(this.props.id).getContext('2d');
+    var chart = new Chart(ctx1, {
+        // The type of chart we want to create
+        type: this.props.type,
+    
+        // The data for our dataset
+        data: {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September'],
+            datasets: [
+              {
+                label: 'Budget',
+                backgroundColor: this.props.color1,
+                borderColor:this.props.color1,
+                data: this.props.data1,
+                fill:false
+              },
+              {
+                label: 'Actual',
+                backgroundColor: this.props.color2,
+                borderColor: this.props.color2,
+                data: this.props.data2,
+                fill:false
+              }]
+        },
+    
+        // Configuration options go here
+        options: {
+          title: {
+            display: true,
+            text: this.props.title
+          },
+          responsive:true,
+          maintainAspectRatio: false,
+          animation:{
+            easing:"easeOutQuint",
+            duration: 1,
+          }
+        }
+    });
+  }
+  
+  render(){
+    return(
+      null
+    );
+  }
+}
+
 class ProgressBarV2 extends React.Component{
   constructor(props) {
     super(props);
@@ -70,21 +166,23 @@ class ProgressBarV2 extends React.Component{
       bar1:props.bar1,
       bar2:props.bar2,
       meter:props.meter
-
     };
+    
     
   }
 
   tick() {
+    if(this.state.todate<=this.state.total){
     this.setState(prevState => ({
       todate: prevState.todate + 1,
       bar1:{ ...this.state.bar1, width:(prevState.todate/prevState.total).toLocaleString("en", {style: "percent"})},
       bar2:{ ...this.state.bar2, width:((prevState.total-prevState.todate)/prevState.total).toLocaleString("en", {style: "percent"}).toLocaleString("en", {style: "percent"})}
     }));
   }
+  }
 
   componentDidMount() {
-    this.interval = setInterval(() => this.tick(), 1000);
+    this.interval = setInterval(() => this.tick(), 100);
   }
 
   componentWillUnmount() {
@@ -99,11 +197,8 @@ class ProgressBarV2 extends React.Component{
         {this.state.bar1.width}
       </div>
       <div style={this.state.bar2}>
-        {this.state.bar2.width}
+      <showPercent num={this.props.bar2.width} />
       </div>
-      
-      
-        
       </div>
       <button onClick={()=>this.componentDidMount()}>Start</button>
       <button onClick={()=>this.componentWillUnmount()}>Stop</button>
@@ -139,6 +234,12 @@ class ProgressBar extends React.Component{
   }
 }
 
+function showPercent(props){
+  if(props.num !== "0%"){
+    console.Log("lalala");
+    return <h2>props.num</h2>;
+  }
+}
 
 
 const redbar =  {
@@ -174,7 +275,7 @@ var bluebar =  {
   backgroundColor: "#66ffed",
 };
 var orangebar = {
-  "border-radius": "10px 0px 0px 10px",
+  "border-radius": "10px",
   display: "inline-block",
   backgroundColor: "#ffab66",
 };
@@ -183,10 +284,11 @@ const meter = {
   "border-radius": "10px",
   border: "solid black 1px",
   margin: "5px",
+  backgroundColor: "#66ffed"
 };
 
 const container = {
-  border:"1px solid black",
+  // border:"1px solid black",
   width: "80%",
   margin: "auto"
 }
