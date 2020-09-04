4a209ebf679c1200fb11dde0bc308c33e62d55f6
aureliano75 <61304559+aureliano75@users.noreply.github.com>
2020-09-04 09:33:33 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,72 +1,144 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 
 function App() {
   return (
+    <div>
+
     <Container />
-    
-    
+    </div>
+
+  );
+}
+
+function H1(props){
+  return(
+  <h1>{props.text}</h1>
   );
 }
 
 function Container(){
   return (
     <div style={container}>
-      <ProgressBar
-        redbar={redbar}
-        greenbar={greenbar}
-        meter={meter}
-        percentage="66%"
+      <H1 
+        text={"Hello World"}  
       />
+      <ul>
+        {list}
+      </ul>
+      <ProgressBar
+        bar1={greenbar}
+        bar2={redbar}
+        meter={meter}
+        todate="10"
+        total="300"
+      />
+      <ProgressBar
+        bar1={purplebar}
+        bar2={yellowbar}
+        meter={meter}
+        todate="11"
+        total="300"
+      />
+      <ProgressBarV2 
+        bar2={bluebar}
+        bar1={orangebar}
+        meter={meter}
+        todate={11}
+        total="300"
+      />
+
+      
+
     </div>
   )
 }
 
+const listelements = ["Dashboard","Other"];
+
+const list = listelements.map((listelement)=>
+<li>{listelement}</li>
+);
+
+class ProgressBarV2 extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = { 
+      todate: props.todate,
+      total : props.total,
+      bar1:props.bar1,
+      bar2:props.bar2,
+      meter:props.meter
+
+    };
+    
+  }
+
+  tick() {
+    this.setState(prevState => ({
+      todate: prevState.todate + 1,
+      bar1:{ ...this.state.bar1, width:(prevState.todate/prevState.total).toLocaleString("en", {style: "percent"})},
+      bar2:{ ...this.state.bar2, width:((prevState.total-prevState.todate)/prevState.total).toLocaleString("en", {style: "percent"}).toLocaleString("en", {style: "percent"})}
+    }));
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return (
+      <div>
+      <div style={this.state.meter}>
+      <div style={this.state.bar1}>
+        {this.state.bar1.width}
+      </div>
+      <div style={this.state.bar2}>
+        {this.state.bar2.width}
+      </div>
+      
+      
+        
+      </div>
+      <button onClick={()=>this.componentDidMount()}>Start</button>
+      <button onClick={()=>this.componentWillUnmount()}>Stop</button>
+      </div>
+    );
+  }
+}
+
+
+
+
+
 class ProgressBar extends React.Component{
   constructor(props){
     super(props);
-    this.state = {};
-    var percentage = props.percentage;
-    props.greenbar.width = percentage;
-    var remaining = (1 - parseFloat(props.greenbar.width)/100).toLocaleString("en", {style: "percent"});
-    props.redbar.width = remaining;
+    var percentage = (this.props.todate/this.props.total).toLocaleString("en", {style: "percent"});
+    this.props.bar1.width = percentage;
+    var remaining = (1 - parseFloat(this.props.bar1.width)/100).toLocaleString("en", {style: "percent"});
+    this.props.bar2.width = remaining;
   }
 
   render(){
     return(
     <div style={this.props.meter}>
-      <div style={this.props.greenbar}>
-        Completion this.{this.props.greenbar.width}
+      <div style={this.props.bar1}>
+        {this.props.bar1.width}
       </div>
-      <div style={this.props.redbar}>
-        Remaining {this.remaining}
+      <div style={this.props.bar2}>
+        {this.props.bar2.width}
       </div>
     </div>
     );
   }
 }
 
-
-const reactstart = (
-<div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-);
 
 
 const redbar =  {
@@ -81,6 +153,30 @@ const greenbar = {
   display: "inline-block",
   backgroundColor: "#42f578",
   width: ""
+};
+const yellowbar =  {
+  "border-radius": "0px 10px 10px 0px",
+  display: "inline-block",
+  backgroundColor: "#fcf262",
+  width: ""
+};
+
+const purplebar = {
+  "border-radius": "10px 0px 0px 10px",
+  display: "inline-block",
+  backgroundColor: "#e562fc",
+  width: ""
+};
+
+var bluebar =  {
+  "border-radius": "0px 10px 10px 0px",
+  display: "inline-block",
+  backgroundColor: "#66ffed",
+};
+var orangebar = {
+  "border-radius": "10px 0px 0px 10px",
+  display: "inline-block",
+  backgroundColor: "#ffab66",
 };
 
 const meter = {
